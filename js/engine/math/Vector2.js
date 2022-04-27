ebfc88556017b1a3df8926f930973c53cd55b624
Ricoo <hegedus.e77@gmail.com>
2022-04-27 21:56:16 +0200

    feat: improve Vector2

--- js/engine/math/Vector2.js
+++ js/engine/math/Vector2.js
@@ -29,17 +29,36 @@ export default class Vector2 {
     }
 
     /**
+     * @desc multiplies the vector by provided value
+     * @param {number} number the value to multiply the vector with
+     * @returns {Vector2} the updated vector
+     */
+    mult(number) {
+        this.x *= number;
+        this.y *= number;
+        return this
+    }
+
+    /**
      * @desc returns a fresh copy of this Vector2
      * @returns {Vector2}
      */
     get copy() {return new Vector2(this.x, this.y);}
 
     /**
+     * @desc returns the length this Vector2
+     * @returns {number}
+     */
+     get length() {return Math.sqrt(this.x**2 + this.y**2)}
+
+    /**
      * @desc returns a directional vector with values between -1 and 1
      * @returns {Vector2}
      */
-    get normalize() {return new Vector2(
-        (this.x > 0 ? 1 : (this.x < 0 ? -1 : 0)),
-        (this.y > 0 ? 1 : (this.y < 0 ? -1 : 0)));
+    get normalize() {
+        if (this.x === 0 && this.y === 0) {
+            return new Vector2(0,0);
+        }
+        return new Vector2(this.x, this.y).mult(1/this.length);
     }
 };
