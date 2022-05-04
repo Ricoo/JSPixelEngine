7d94eb74b7c37f5c67a79e3c01de1a3daa3db746
Ricoo <hegedus.e77@gmail.com>
2022-05-05 00:40:26 +0200

    feat: add lineWidth to path tag

--- js/resource/sprite/Path.js
+++ js/resource/sprite/Path.js
@@ -14,7 +14,7 @@ export default class Path {
     }
 
     get(x, y, lineWidth, strokeColor=this._stroke, fillColor=this._fill) {
-        let name = `${this._name}:${x}x${y}:${strokeColor}:${fillColor}`
+        let name = `${this._name}:${x}x${y}:${lineWidth}:${strokeColor}:${fillColor}`
         let sprite = ResourceManager.getSprite(name);
         if (sprite) {
             return name;
